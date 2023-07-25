f5d6b6a9705dd0a09561edc5001c4244a260130e
jojomumu <jonathan.robert.pro30@gmail.com>
2023-07-25 11:59:44 +0200

    finally done with locale storage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ const toggleDark = document.getElementById('buttdark');
 const body = document.body;
 const logo = document.getElementById('logo');
 
-toggleDark.addEventListener('click', function() {
+toggleDark.addEventListener('click', function () {
   body.classList.toggle('darkmode');
   const isDarkMode = body.classList.contains('darkmode');
-
-
 
   if (isDarkMode) {
     logo.src = 'media/logo.png';
@@ -29,9 +27,7 @@ toggleDark.addEventListener('click', function() {
   }
 });
 
-
 const addButtons = document.querySelectorAll('#add1');
-
 
 addButtons.forEach((button) => {
   button.addEventListener('click', () => {
@@ -44,7 +40,7 @@ addButtons.forEach((button) => {
 
     const descriptionInput = document.createElement('textarea');
 
-    descriptionInput.className = 'description'
+    descriptionInput.className = 'description';
     descriptionInput.placeholder = 'Task description';
 
     const dateInput = document.createElement('input');
@@ -109,7 +105,6 @@ addButtons.forEach((button) => {
       taskContainer.appendChild(deleteButton);
       taskDetails.appendChild(taskContainer);
 
-
       const descriptionSpan = document.createElement('span');
 
       descriptionSpan.className = 'taskdescri';
@@ -129,128 +124,109 @@ addButtons.forEach((button) => {
       form.reset();
       form.remove();
 
-    
-      deleteButton.addEventListener('click', deleteTask);
+      
+      const task = {
+        name: taskName,
+        description: taskDescription,
+        date: taskDate,
+        status: 'todo',
+      };
 
+      let tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
+      tasksArray.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasksArray));
+
+      deleteButton.addEventListener('click', deleteTask);
     });
   });
 });
 
-
 function deleteTask() {
   const taskItem = this.closest('.lili');
   taskItem.remove();
+
+  
+  const taskName = taskItem.querySelector('.taskname').textContent;
+  let tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasksArray = tasksArray.filter((task) => task.name !== taskName);
+  localStorage.setItem('tasks', JSON.stringify(tasksArray));
 }
 
-
 const tasks = document.querySelectorAll('.lili');
-tasks.forEach(task => {
+tasks.forEach((task) => {
   task.addEventListener('dragstart', dragStart);
   task.addEventListener('dragend', dragEnd);
 });
-
 
 function dragStart() {
   currentTask = this;
   currentTaskList = this.parentNode;
   this.classList.add('dragging');
- 
+
   const nameSpan = this.querySelector('.taskname');
   const deleteButton = this.querySelector('.deletebutton');
 
   event.dataTransfer.setData('text/plain', '');
 }
 
-
 function dragEnd() {
   currentTask = null;
   currentTaskList = null;
- 
+
   this.classList.remove('dragging');
- 
 }
 
-
 const sections = document.querySelectorAll('#todo, #doing, #done');
-sections.forEach(section => {
+sections.forEach((section) => {
   section.addEventListener('dragover', dragOver);
   section.addEventListener('dragenter', dragEnter);
   section.addEventListener('dragleave', dragLeave);
   section.addEventListener('drop', drop);
 });
 
-
 function dragOver(e) {
   e.preventDefault();
 }
-
 
 function dragEnter(e) {
   e.preventDefault();
   this.classList.add('dragover');
 }
 
-
 function dragLeave() {
   this.classList.remove('dragover');
 }
-
 
 function drop() {
   this.classList.remove('dragover');
   const targetList = this.querySelector('ul');
   if (targetList === currentTaskList) {
-   
     targetList.appendChild(currentTask);
   } else {
-   
     currentTaskList.removeChild(currentTask);
- 
     targetList.appendChild(currentTask);
+
+    
+    const tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
+    const taskName = currentTask.querySelector('.taskname').textContent;
+    const taskIndex = tasksArray.findIndex((task) => task.name === taskName);
+    if (taskIndex !== -1) {
+      if (targetList.id === 'todo-list') {
+        tasksArray[taskIndex].status = 'todo';
+      } else if (targetList.id === 'doing-list') {
+        tasksArray[taskIndex].status = 'doing';
+      } else if (targetList.id === 'done-list') {
+        tasksArray[taskIndex].status = 'done';
+      }
+      localStorage.setItem('tasks', JSON.stringify(tasksArray));
+    }
   }
 }
 
-function saveTasksToLocalStorage() {
-  const tasks = document.querySelectorAll('.lili');
-  const tasksData = [];
-
-  tasks.forEach(task => {
-    const nameSpan = task.querySelector('.taskname');
-    const descriptionSpan = task.querySelector('.taskdescri');
-    const dateSpan = task.querySelector('.taskdate');
-
-    const taskListElement = task.closest('section');
-    const taskListId = taskListElement ? taskListElement.id : null;
-
-    tasksData.push({
-      taskName: nameSpan.textContent,
-      taskDescription: descriptionSpan.textContent,
-      taskDate: dateSpan.textContent,
-      taskListId: taskListId,
-    });
-  });
-
-  localStorage.setItem('tasksData', JSON.stringify(tasksData));
-}
-
-
-const todoList = document.getElementById('todo-list');
-const doingList = document.getElementById('doing-list');
-const doneList = document.getElementById('done-list');
-
-todoList.addEventListener('DOMNodeInserted', saveTasksToLocalStorage);
-todoList.addEventListener('DOMNodeRemoved', saveTasksToLocalStorage);
-doingList.addEventListener('DOMNodeInserted', saveTasksToLocalStorage);
-doingList.addEventListener('DOMNodeRemoved', saveTasksToLocalStorage);
-doneList.addEventListener('DOMNodeInserted', saveTasksToLocalStorage);
-doneList.addEventListener('DOMNodeRemoved', saveTasksToLocalStorage);
-
-
 function loadTasksFromLocalStorage() {
-  const tasksData = JSON.parse(localStorage.getItem('tasksData'));
-  if (!tasksData) return;
+  const tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
 
-  tasksData.forEach(taskData => {
+  tasksArray.forEach((task) => {
     const taskItem = document.createElement('li');
     taskItem.className = 'lili';
     taskItem.draggable = true;
@@ -265,44 +241,41 @@ function loadTasksFromLocalStorage() {
     const deleteButton = document.createElement('button');
     deleteButton.className = 'deletebutton';
     deleteButton.innerHTML = '<img src="media/cross.png" alt="Delete" width="20" height="20">';
-    deleteButton.addEventListener('click', deleteTask);
 
     const taskContainer = document.createElement('div');
     taskContainer.className = 'taskcontainer';
 
     const nameSpan = document.createElement('span');
     nameSpan.className = 'taskname';
-    nameSpan.textContent = taskData.taskName;
+    nameSpan.textContent = task.name;
 
-    const descriptionSpan = document.createElement('span');
-    descriptionSpan.className = 'taskdescri';
-    descriptionSpan.textContent = taskData.taskDescription;
-
-    const dateSpan = document.createElement('span');
-    dateSpan.className = 'taskdate';
-    dateSpan.textContent = taskData.taskDate;
+    deleteButton.addEventListener('click', deleteTask);
 
     taskContainer.appendChild(nameSpan);
     taskContainer.appendChild(deleteButton);
     taskDetails.appendChild(taskContainer);
+
+    const descriptionSpan = document.createElement('span');
+    descriptionSpan.className = 'taskdescri';
+    descriptionSpan.textContent = task.description;
+
+    const dateSpan = document.createElement('span');
+    dateSpan.className = 'taskdate';
+    dateSpan.textContent = task.date;
+
     taskDetails.appendChild(descriptionSpan);
     taskDetails.appendChild(dateSpan);
 
     taskItem.appendChild(taskDetails);
 
-    const taskListId = taskData.taskListId;
-
-    if (taskListId === 'todo') {
-      const todoList = document.getElementById('todo-list');
-      todoList.appendChild(taskItem);
-    } else if (taskListId === 'doing') {
-      const doingList = document.getElementById('doing-list');
-      doingList.appendChild(taskItem);
-    } else if (taskListId === 'done') {
-      const doneList = document.getElementById('done-list');
-      doneList.appendChild(taskItem);
+    if (task.status === 'todo') {
+      document.getElementById('todo-list').appendChild(taskItem);
+    } else if (task.status === 'doing') {
+      document.getElementById('doing-list').appendChild(taskItem);
+    } else if (task.status === 'done') {
+      document.getElementById('done-list').appendChild(taskItem);
     }
   });
 }
 
-window.onload = loadTasksFromLocalStorage;
+window.addEventListener('load', loadTasksFromLocalStorage);
